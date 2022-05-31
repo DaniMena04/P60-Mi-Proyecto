12bbf4b48d8d6277715f279752df06213220a21f
Daniel Mena <dmenae@est.ups.edu.ec>
2022-05-31 10:34:38 -0500

    Crear clase Persona

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
+#include "persona.h"
 using namespace std;
 
 int main()
 {
     cout << "Buena noche jsjsjs!" << endl;
+    Persona p;
+
+    p.nombre = "Daniel";
+    p.edad = 39;
+
+    cout << p.nombre << endl;
+    cout << p.edad << endl;
     return 0;
 }
---
+++ persona.h
@@ -0,0 +1,16 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+
+#include <string>
+
+using namespace std;
+
+class Persona
+{
+public:
+    Persona();
+    string nombre;
+    int edad;
+};
+
+#endif // PERSONA_H
